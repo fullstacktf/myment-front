fb6e044c2b43f379bdf4dd4fb416839fc4cd20ba
pabloderivia <htorrespablo@gmail.com>
2019-07-17 23:22:42 +0100

    functional loggedHeader and newsBar based on a input-string

---
+++ src/components/LoggedHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="headerNav">
+        <img src="../assets/M.png" alt="our coolest logo">
+        <newsBar class = "news"></newsBar>
+    </div>
+</template>
+
+<script>
+import newsBar from './NewsBar';
+export default {
+    components:{
+        'newsBar': newsBar
+    }
+
+}
+</script>
+
+<style>
+.news{
+    height: 60px;
+}
+.headerNav{
+    display:flex;
+    border: 1px solid black;
+}
+.headerNav img {
+    border: 1px solid black;
+    width: 60px;
+    height: 60px;
+}
+
+</style>
---
+++ src/components/NewsBar.vue
@@ -0,0 +1,47 @@
+<template> <!-- Just a cool way to show our news -->
+    <div class="containerBar marquee" >
+        <h6 >{{newNews}}</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        newNews: {
+            type: String,
+            required: true,
+            default: "if you are watching it, some error may has happened or its just a random text for test it. Use the 'newNews' prop for change me. We will show in this bar some useful stuff"
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+.marquee {
+    display:flex;
+    align-items: center;
+    font-size: 20px;
+    width: 100%;
+    height: 50px;
+	background-color: rgb(0, 0, 0);
+	color: white;
+    white-space: nowrap;
+    overflow: hidden;
+    box-sizing: border-box;
+    border: double 3px grey;
+    box-shadow: inset 3px 3px 4px white, inset -3px -3px 4px white;
+}
+.marquee h6 {
+    display: inline-block;
+    padding-left: 100%;
+    animation: marquee 15s linear infinite;
+}
+@keyframes marquee {
+    0%   { transform: translate(0, 0); }
+    100% { transform: translate(-100%, 0); }
+}
+
+</style>
