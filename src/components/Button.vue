27cbd8e4da138537276fe73095f405faf8935fce
Eric Durr <eric.d.s.info@gmail.com>
2019-07-29 19:26:02 +0100

    passed the components to the true develop mirror

---
+++ src/components/Button.vue
@@ -0,0 +1,70 @@
+<template>
+    <button class="Button" > {{name}}</button>
+</template>
+
+<script>
+export default {
+
+  props: {
+    name: { //Generate, add or examine
+      type: String,
+      required: true,
+      default: 'generate'
+    },
+      route: {
+      type: String,
+      required: false,
+      default: ''
+    },
+  
+
+
+  },
+  name: 'Button', 
+  data() {
+    return {
+   }
+  },
+  methods: {
+    Redirect(){
+      
+    }
+  }
+
+  }
+
+</script>
+
+<style > 
+.Button {
+  color:white;
+  background: #454647;
+  background-image: -webkit-linear-gradient(top, #454647, #000000);
+  background-image: -moz-linear-gradient(top, #454647, #000000);
+  background-image: -ms-linear-gradient(top, #454647, #000000);
+  background-image: -o-linear-gradient(top, #454647, #000000);
+  background-image: linear-gradient(to bottom, #454647, #000000);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  font-family: Arial;
+  font-size: 20px;
+  padding: 10px 20px 10px 20px;
+  text-decoration: none;
+  box-shadow: 2px 2px #454647;
+
+}
+.Button:hover{
+  color: white;
+  background: #061636;
+  background-image: -webkit-linear-gradient(top, #061636, #092038);
+  background-image: -moz-linear-gradient(top, #061636, #092038);
+  background-image: -ms-linear-gradient(top, #061636, #092038);
+  background-image: -o-linear-gradient(top, #061636, #092038);
+  background-image: linear-gradient(to bottom, #061636, #092038);
+  text-decoration: none;  
+}
+
+</style>
+
+  
---
+++ src/components/SingleMenuLink.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="fastLink">{{name}}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { //Generate, add or examine
+      type: String,
+      required: true,
+      default: 'generate short'
+    },
+      route: {
+      type: String,
+      required: false,
+      default: ''
+    },
+
+  }, 
+
+  data() {
+    return {
+   }
+  }
+
+  }
+
+  
+
+</script>
+
+<style > 
+.fastLink {
+    color: white;
+    background: black;
+    border-radius: 60px;
+    width: 100%;
+    text-align: center;
+}
+</style>
+
+
