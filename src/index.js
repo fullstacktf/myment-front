f26862ec60818dcf6ab6abe609a66b03cb67c401
Eric Durr <eric.d.s.info@gmail.com>
2019-08-02 19:11:18 +0100

    deleted dependencies for semantic

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
-import SuiVue from 'semantic-ui-vue';
-import 'semantic-ui-css/semantic.min.css';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 
 Vue.use(Buefy);
-
-Vue.use(SuiVue);
 
 new Vue({
 	store,
