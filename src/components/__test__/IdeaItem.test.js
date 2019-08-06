d56ed3397306e441977d99d02ab478b87b6b9a82
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-06 04:42:42 +0100

    :sunny: Test IdeaAdder component with success

---
+++ src/components/__test__/IdeaItem.test.js
@@ -0,0 +1,57 @@
+import IdeaAdder from '../IdeaItem.vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Buefy from 'buefy';
+import { mockData } from './mockData';
+
+const mock = {
+  name: mockData.zone[0].food.ideas[0].name,
+  description: mockData.zone[0].food.ideas[0].description,
+  start: mockData.zone[0].food.ideas[0].startTime[0],
+  end: mockData.zone[0].food.ideas[0].endTime[0],
+};
+describe('IdeaAdder', () => {
+  let localVue;
+  beforeEach(() => {
+    const Vue = createLocalVue();
+    Vue.use(Buefy);
+    localVue = Vue;
+  });
+  test('render component', () => {
+    const wrapper = shallowMount(IdeaAdder, {
+      localVue,
+      propsData: {
+        ...mock,
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('check button props', () => {
+    const wrapper = shallowMount(IdeaAdder, {
+      localVue,
+      propsData: {
+        ...mock,
+      },
+    });
+    const button = wrapper.find('button');
+    expect(button.text()).toBe('Guachinche Ivan');
+  });
+
+  test('check Contents props', () => {
+    const wrapper = shallowMount(IdeaAdder, {
+      localVue,
+      propsData: {
+        ...mock,
+      },
+    });
+    const content = wrapper.find('.content');
+    const Time = content.findAll('h3');
+    const description = content.find('p');
+
+    expect(Time.at(0).text()).toBe('9');
+    expect(Time.at(1).text()).toBe('11');
+    expect(description.text()).toBe(
+      'Go to Guachinche Ivan in La Laguna and take a rabbit',
+    );
+  });
+});
--- src/views/Short_generator/__test__/ShortGenerator.test.js
+++ src/views/Short_generator/__test__/ShortGenerator.test.js
@@ -2,7 +2,13 @@ import ShortGenerator from '../ShortGenerator.vue';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import Buefy from 'buefy';
-
+import { mockData } from './mockData';
+const mock = {
+  name: mockData.zone[0].food.ideas[0].name,
+  description: mockData.zone[0].food.ideas[0].description,
+  timeStart: mockData.zone[0].food.ideas[0].startTime,
+  timeEnd: mockData.zone[0].food.ideas[0].endTime,
+};
 describe('ShortGenerator', () => {
   let localVue;
   beforeEach(() => {
@@ -12,19 +18,27 @@ describe('ShortGenerator', () => {
     localVue = Vue;
   });
 
-  test('button exist', () => {
-    const wrapper = shallowMount(ShortGenerator, {
-      localVue,
-    });
-    const existButton = wrapper.find('button');
-    expect(existButton).toBeTruthy();
-    expect();
-  });
   test('render button', () => {
     const wrapper = shallowMount(ShortGenerator, {
       localVue,
     });
-
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('button exist', () => {
+    const wrapper = shallowMount(ShortGenerator, {
+      localVue,
+    });
+    const existButton = wrapper.find('.button');
+    expect(existButton).toBeTruthy();
+  });
+
+  test('Check the loggedAdded', () => {
+    const wrapper = shallowMount(ShortGenerator, {
+      localVue,
+      propsData: {
+        ideaStructure: mock,
+      },
+    });
   });
 });
