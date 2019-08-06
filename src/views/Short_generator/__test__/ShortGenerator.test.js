9ee3899ca43228deadfcf689d8bc9c8519d4dcc5
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-06 02:37:26 +0100

    Merge with develop

--- src/views/Short_generator/__test__/ShortGenerator.test.js
+++ src/views/Short_generator/__test__/ShortGenerator.test.js
@@ -1,5 +1,4 @@
 import ShortGenerator from '../ShortGenerator.vue';
-<<<<<<< HEAD
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import Buefy from 'buefy';
@@ -28,23 +27,4 @@ describe('ShortGenerator', () => {
 
     expect(wrapper.html()).toMatchSnapshot();
   });
-=======
-import { shallowMount } from '@vue/test-utils';
-import { mockData } from './mockData';
-
-describe('ShortGenerator', () => {
-  test('button exist', () => {
-    const wrapper = shallowMount(ShortGenerator);
-    const existButton = wrapper.find('button');
-    expect(existButton).toBeTruthy();
-  }),
-    test('render button', () => {
-      const wrapper = shallowMount(ShortGenerator);
-      expect(wrapper.html()).toMatchSnapshot();
-    }),
-    test('Data exist', () => {
-      const data = mockData;
-      expect(data).toBe(1);
-    });
->>>>>>> 1863ff2744c261473b8e58a14d87bdcc23910c70
 });
