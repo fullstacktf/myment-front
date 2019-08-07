5e01a69fdf19e8f6c87750e949330efbc86fd2ba
Eric Durr <eric.d.s.info@gmail.com>
2019-08-07 15:19:27 +0100

    started skeleton

--- src/views/Home_view/Home.vue
+++ src/views/Home_view/Home.vue
@@ -2,23 +2,54 @@
   <div class="homeWrapper">
     <div class="homeHeader">
       <div class="columns">
-        <div class="column is-1"></div>
-        <div class="column"></div>
+        <div class="column is-2 is-fullwidth">
+          <figure class="image is-128x128 centering">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </figure>
+        </div>
+        <div class="column is-10 centering">
+          <news-bar :newNews="hello"></news-bar>
+        </div>
       </div>
     </div>
-    <div class="homeContainer"></div>
+    <div class="homeContainer">
+      <div class="columns">
+        <div class="column is-3"></div>
+        <div class="column is-6"></div>
+        <div class="column is-3"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NewsBar from '../../components/NewsBar';
+
 export default {
   name: 'Home',
+  components: {
+    'news-bar': NewsBar,
+  },
 };
 </script>
 
 <style>
-.homeWrapper {
-  display: grid;
-  grid-template-rows: 10% 90%;
+.homeHeader {
+  display: flex;
+  min-height: 15%;
+  background-color: #cae0cf;
+  box-shadow: -40px -40px black;
+}
+.homecontainer {
+  min-height: 85%;
+}
+
+.centering {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.news {
 }
 </style>
