708fda9ce1aebce43eca7a1697e2d1e1fa9d2a7a
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-08 13:24:04 +0100

    Try webpack output different

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   },
   output: {
-    path: path.resolve(__dirname + '/dist'),
+    path: __dirname,
     publicPath: '/',
     filename: 'index.js',
     chunkFilename: '[name].[hash].js',
