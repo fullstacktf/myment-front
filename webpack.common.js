33d5a0d5b813f15c3e3c284da17d7847cda1a423
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-08 14:53:22 +0100

    Added need folder

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   },
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname + '/dist'),
     publicPath: '/',
     filename: 'index.js',
     chunkFilename: '[name].[hash].js',
