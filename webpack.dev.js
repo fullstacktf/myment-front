1d9a85593a9b113e5488ae7865819e48ee7534e8
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-08 03:23:49 +0100

    :green_heart: Added webpack production configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,32 +1,20 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlPlugin = require('html-webpack-plugin');
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
-
-
+const path = require('path');
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    watch: true,
-    plugins: [
-        new HtmlPlugin({
-            title: 'Develop',
-            template: 'public/index.html',
-            chunksSortMode: 'dependency'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new FriendlyErrorsPlugin()
-    ],
-    devServer: {
-        contentBase: path.join('/dist/'),
-        compress: true,
-        historyApiFallback: true,
-        hot: true,
-        open: true,
-        overlay: true,
-        port: 8000,
-        stats: {
-            normal: true
-        }
+  mode: 'development',
+  devtool: 'source-map',
+  watch: true,
+  devServer: {
+    contentBase: path.resolve(__dirname + '/dist'),
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    overlay: true,
+    port: 8000,
+    stats: {
+      normal: true,
     },
+  },
 });
