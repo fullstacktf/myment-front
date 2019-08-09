aea535fcf5c1c3206a04067e66337ca9ae2f0008
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-09 02:50:33 +0100

    Added public path

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname + '/dist'),
-    publicPath: '/',
+    publicPath: 'myment-front',
     filename: 'js/index.js',
     chunkFilename: 'js/[name].[hash].js',
   },
