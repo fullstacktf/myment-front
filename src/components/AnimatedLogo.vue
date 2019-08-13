765f16083a6013918e5c0aad403fce961966e604
Eric Durr <eric.d.s.info@gmail.com>
2019-08-13 16:33:21 +0100

    added new icon and logo parts

--- src/components/AnimatedLogo.vue
+++ src/components/AnimatedLogo.vue
@@ -1,4 +1,8 @@
-<template></template>
+<template>
+<div class="logoWrapper">
+<img class="hero-logo-circle" src="../src/assets/myment-logo_circle_Mesa de trabajo 1.svg" alt="">
+</div>
+</template>
 
 <script>
 export default {
@@ -7,4 +11,29 @@ export default {
 </script>
 
 <style>
+.hero-logo-circle:nth-child(10) {
+    animation-duration: 120s;
+}
+.hero-logo-circle {
+    position: absolute;
+    left: 0;
+    top: 0;
+    animation: hero-logo-circle 1s linear infinite;
+    animation-duration: 1s;
+    animation-timing-function: linear;
+    animation-delay: 0s;
+    animation-iteration-count: infinite;
+    animation-direction: normal;
+    animation-fill-mode: none;
+    animation-play-state: running;
+    animation-name: hero-logo-circle;
+    will-change: transform;
+}
+
+* {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
 </style>
